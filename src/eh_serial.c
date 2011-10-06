4d3962a7643253653110941d34ef6837bc3dc14f
Alejandro Mery <amery@geeks.cl>
2011-10-05 20:51:57 -0400

    eh_serial: added missing include

--- src/eh_serial.c
+++ src/eh_serial.c
@@ -26,6 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "eh.h"
 #include "eh_serial.h"
 #include "eh_fd.h"
 #include "eh_watcher.h"
